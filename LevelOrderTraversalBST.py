7fe089557321f84e640b2ffa288503d1f9f76138
Nijaz Kovacevic <nijaz.kovacevic94@gmail.com>
2023-01-04 16:47:18 -0800

    Added BST solutions from Leetcode

---
+++ LevelOrderTraversalBST.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        result = []
+
+        # Initialize queue to append nodes from the BST
+        q = collections.deque()
+        q.append(root)
+
+        # BFS traversal
+        while q:
+            qLen = len(q)
+            level = []
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                result.append(level)
+        
+        return result
